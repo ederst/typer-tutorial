2410afd9f2eb6f038ff3db1ae8f5d1ed84432999
ederst <stefan.prietl@bearingpoint.com>
2022-03-03 13:47:17 +0100

    Add CLI option with a value
    https://typer.tiangolo.com/tutorial/first-steps/#a-cli-option-with-a-value

--- typer_intro/cli.py
+++ typer_intro/cli.py
@@ -4,10 +4,11 @@ GREETING = "Hello"
 FORMAL_GREETING = "Good day Mr/Ms"
 
 
-def _main(name: str, last_name: str, formal: bool = False):
+def _main(name: str, last_name: str = "", formal: bool = False):
     greeting = GREETING if not formal else FORMAL_GREETING
+    full_name = f"{name} {last_name}".strip()
 
-    typer.echo(f"{greeting} {name} {last_name}!")
+    typer.echo(f"{greeting} {full_name}!")
 
 
 def main():
