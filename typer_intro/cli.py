8afdaffd2409dc8f5616f53f1bff90b59722a522
ederst <stefan.prietl@bearingpoint.com>
2022-03-03 13:19:55 +0100

    Add some basic tests

--- typer_intro/cli.py
+++ typer_intro/cli.py
@@ -1,10 +1,13 @@
 import typer
 
+GREETING = "Hello"
+FORMAL_GREETING = "Good day Mr/Ms"
+
 
 def _main(name: str, last_name: str, formal: bool = False):
-    greeting_prefix = "Hello" if not formal else "Good day Mr/Ms"
+    greeting = GREETING if not formal else FORMAL_GREETING
 
-    typer.echo(f"{greeting_prefix} {name} {last_name}!")
+    typer.echo(f"{greeting} {name} {last_name}!")
 
 
 def main():
