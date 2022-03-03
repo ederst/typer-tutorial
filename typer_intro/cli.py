be8d77128f5fa5b259574a9f9c236df86cf2b930
ederst <stefan.prietl@bearingpoint.com>
2022-03-03 14:38:07 +0100

    Document the CLI app
    https://typer.tiangolo.com/tutorial/first-steps/#document-your-cli-app

--- typer_intro/cli.py
+++ typer_intro/cli.py
@@ -5,6 +5,12 @@ FORMAL_GREETING = "Good day Mr/Ms"
 
 
 def _main(name: str, last_name: str = "", formal: bool = False):
+    """
+    Say hi to NAME, optionally with a --last-name.
+
+    If --formal is used, say hi very formally.
+    """
+
     greeting = GREETING if not formal else FORMAL_GREETING
     full_name = f"{name} {last_name}".strip()
 
